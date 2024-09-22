46aca09aeed6b2de02fa19ed51dcd002505b7c0e
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-22 12:41:38 +0530

    Update mute.js

--- plugins2/mute.js
+++ plugins2/mute.js
@@ -10,20 +10,19 @@ cmd({
 },
 async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-
-//if (!isOwner || !isAdmins) return;
-//if (!m.isGroup) return reply(mg.onlygroup);
-//if (!isBotAdmins) return reply(mg.needbotadmins);     
-
+   
+    
     if (!isGroup) return reply('This command can only be used in a group❗.')
-    if (!isBotAdmins) return reply('GIVE GROUP ADMIN TO *THENU-MD* ❗.')
-    if (!isAdmins) return reply('IM NOT THE OWNER ❌.')
+        if (!isBotAdmins) return reply('PLEASE GIVE ADMIN TO *THENU - MD* ❗.')
+        if (!isAdmins) return reply('YOU ARE NOT THE OWNER.. ❌.')
+ 
 
+     
             await conn.groupSettingUpdate(m.chat, "announcement")
-           const mass = await conn.sendMessage(m.chat, { text: '*Group chat muted* 🔒' }, { quoted: mek });
+           const mass = await conn.sendMessage(m.chat, { text: '🔒 GROUP IS CLOSED MY BOT OWNER' }, { quoted: mek });
             return await conn.sendMessage(m.chat, { react: { text: '🔒', key: mass.key } });
 } catch(e) {
 console.log(e);
-reply('*Error !!*')    
+reply('*PLEASE GIVE ADMIN TO BOT.. ❗❗*')    
 } 
 })
