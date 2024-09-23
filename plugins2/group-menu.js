ba3665649bbb08c4b9b3624ddac706174a23c6ac
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-23 09:11:22 +0530

    Update group-menu.js

--- plugins2/group-menu.js
+++ plugins2/group-menu.js
@@ -1,1 +1,43 @@
+████████╗██╗  ██╗███████╗███╗   ██╗██╗   ██╗    ███╗   ███╗██████╗ 
+╚══██╔══╝██║  ██║██╔════╝████╗  ██║██║   ██║    ████╗ ████║██╔══██╗
+   ██║   ███████║█████╗  ██╔██╗ ██║██║   ██║    ██╔████╔██║██║  ██║
+   ██║   ██╔══██║██╔══╝  ██║╚██╗██║██║   ██║    ██║╚██╔╝██║██║  ██║
+   ██║   ██║  ██║███████╗██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║██████╔╝
+   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝╚═════╝ 
+                                                                   
+                                                                   
+                                                                   
+                                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function _0x440c(){const _0x4f0e9d=['112196SpvPSp','audio/mp4','*\x0a\x0a>\x20👨‍💻\x20​🇹​​🇭​​🇪​​🇳​​🇺​-​🇲​​🇩​\x20​🇲​​🇪​​🇳​​🇺​👨‍💻\x0a\x0a👨🏻‍💻⚟\x20*ɴᴀᴍᴇ\x20ʙᴏᴛ*\x20:\x20ᵀʰᵉⁿᵘ⁻ᴹᴰ💫\x0a👨🏻‍💻⚟\x20*ᴄʀᴇᴀᴛᴏʀ*\x20:\x20\x20𝓣𝓱𝓮𝓷𝓾𝓵𝓪\x20𝓟𝓪𝓷𝓪𝓹𝓲𝓽𝓲💫\x0a👨🏻‍💻⚟*ᴠᴇʀsɪᴏɴs*\x20:\x206.0.0\x20(ᴀᴅᴅᴇᴅ\x20ʜɪᴅᴅᴇɴ\x20ғᴇᴀᴜᴛᴜʀᴇs)💫\x0a👨🏻‍💻⚟\x20*ᴄᴏɴᴛᴀᴄᴛ*\x20:\x20https://wa.me/94757096717\x20💫\x0a\x0a╭───────────●●►\x0a│📜\x20GROUP\x20COMMANDS\x0a│───────\x0a','length','3797736kCcuCk','log','6bUOZiH','group','8NgTvyl','https://github.com/prabathLK/AUTO-VOICE-SENDER-PRABATH-MD-/raw/refs/heads/main/plugins/bgm/AUD-20221018-WA0232.mp3','4100202piarLm','sendMessage','https://i.ibb.co/6ZJFnSP/group.png','pattern','1174278qHgTct','\x0a*👋\x20Hello\x20','get\x20bot\x20group\x20cmd.','10KchFIX','PREFIX','dontAddCommandList','23413093eACtyf','37xWuUgr','1814275DqCtjT','28442nksLRu'];_0x440c=function(){return _0x4f0e9d;};return _0x440c();}const _0x100a60=_0x5690;(function(_0x3bad65,_0x4f1961){const _0x2300c1=_0x5690,_0x85c490=_0x3bad65();while(!![]){try{const _0x4b2c8e=-parseInt(_0x2300c1(0x1b5))/0x1*(parseInt(_0x2300c1(0x1b7))/0x2)+parseInt(_0x2300c1(0x1ae))/0x3*(-parseInt(_0x2300c1(0x1c0))/0x4)+-parseInt(_0x2300c1(0x1b6))/0x5*(parseInt(_0x2300c1(0x1be))/0x6)+parseInt(_0x2300c1(0x1b8))/0x7+-parseInt(_0x2300c1(0x1bc))/0x8+parseInt(_0x2300c1(0x1c2))/0x9*(parseInt(_0x2300c1(0x1b1))/0xa)+parseInt(_0x2300c1(0x1b4))/0xb;if(_0x4b2c8e===_0x4f1961)break;else _0x85c490['push'](_0x85c490['shift']());}catch(_0x1ea595){_0x85c490['push'](_0x85c490['shift']());}}}(_0x440c,0x6eb5c));const {readEnv}=require('../lib/database'),{cmd,commands}=require('../command');function _0x5690(_0x3f3630,_0x5c4974){const _0x440c40=_0x440c();return _0x5690=function(_0x569079,_0x4ec705){_0x569079=_0x569079-0x1ae;let _0x44c276=_0x440c40[_0x569079];return _0x44c276;},_0x5690(_0x3f3630,_0x5c4974);}cmd({'pattern':'groupmenu','desc':_0x100a60(0x1b0),'category':_0x100a60(0x1bf),'react':'💻','filename':__filename},async(_0x4a2914,_0x3531ac,_0x386f4d,{from:_0x4c660e,quoted:_0x22fa2d,body:_0x1bcc8a,isCmd:_0x5cf8d3,command:_0xd9e65b,args:_0x45090a,q:_0x2183fd,isGroup:_0x3e16ba,sender:_0xc2125c,senderNumber:_0x531590,botNumber2:_0x780c8f,botNumber:_0x50081b,pushname:_0x2822e3,isMe:_0xb570c,isOwner:_0x4dbc32,groupMetadata:_0x48c066,groupName:_0x2b6b50,participants:_0x30e324,groupAdmins:_0x1a9fdf,isBotAdmins:_0x8e7d2,isAdmins:_0x367d2a,reply:_0xd938ac})=>{const _0x4403fe=_0x100a60;try{const _0x3e1053=await readEnv();let _0x476fe9={'group':''};for(let _0xcac188=0x0;_0xcac188<commands[_0x4403fe(0x1bb)];_0xcac188++){commands[_0xcac188][_0x4403fe(0x1c5)]&&!commands[_0xcac188][_0x4403fe(0x1b3)]&&(_0x476fe9[commands[_0xcac188]['category']]+=''+_0x3e1053[_0x4403fe(0x1b2)]+commands[_0xcac188]['pattern']+'\x0a');}let _0x9b9611=_0x4403fe(0x1af)+_0x2822e3+_0x4403fe(0x1ba)+_0x476fe9['group']+'\x0a╰───────────●●►\x0a\x20\x20\x20\x20';await _0x4a2914[_0x4403fe(0x1c3)](_0x4c660e,{'audio':{'url':_0x4403fe(0x1c1)},'mimetype':_0x4403fe(0x1b9),'ptt':!![]},{'quoted':_0x3531ac}),await _0x4a2914['sendMessage'](_0x4c660e,{'image':{'url':_0x4403fe(0x1c4)},'caption':_0x9b9611},{'quoted':_0x3531ac});}catch(_0xf7bbe){console[_0x4403fe(0x1bd)](_0xf7bbe),_0xd938ac(''+_0xf7bbe);}});
