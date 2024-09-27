f93f99dcbbc84e4262c9f387bd4ca75879057e05
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-27 17:08:15 +0530

    Update main-menu.js

--- plugins2/main-menu.js
+++ plugins2/main-menu.js
@@ -1,1 +1,32 @@
+//████████╗██╗  ██╗███████╗███╗   ██╗██╗   ██╗    ███╗   ███╗██████╗ 
+//╚══██╔══╝██║  ██║██╔════╝████╗  ██║██║   ██║    ████╗ ████║██╔══██╗
+//   ██║   ███████║█████╗  ██╔██╗ ██║██║   ██║    ██╔████╔██║██║  ██║
+//   ██║   ██╔══██║██╔══╝  ██║╚██╗██║██║   ██║    ██║╚██╔╝██║██║  ██║
+//   ██║   ██║  ██║███████╗██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║██████╔╝
+//   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝╚═════╝ 
+                                                                   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function _0x5026(){const _0x3a2eac=['342TDjUfB','6675uFWGsp','sendMessage','dontAddCommandList','https://github.com/prabathLK/AUTO-VOICE-SENDER-PRABATH-MD-/raw/refs/heads/main/plugins/bgm/AUD-20221018-WA0251.mp3','BOT_NAME','pattern','category','77BunpBp','21192AmnWkW','main','632NUykYX','5368nmPcxK','\x0a*👤\x20Owner:*\x20Thenula..\x0a*📱\x20Bot\x20Number:*\x20wa.me/','506852YHwKKt','PREFIX','40700pUOsCl','1034034SqiZgu','*\x0a*📝\x20Bot\x20Name:*\x20','1PnnhqI','https://i.ibb.co/J5FHD4j/MAIN.png','\x0a*📝\x20Commands:*156\x0a\x0a\x0a👨🏻‍💻⚟\x20*ɴᴀᴍᴇ\x20ʙᴏᴛ*\x20:\x20ᵀʰᵉⁿᵘ⁻ᴹᴰ💫\x0a👨🏻‍💻⚟\x20*ᴄʀᴇᴀᴛᴏʀ*\x20:\x20\x20𝓣𝓱𝓮𝓷𝓾𝓵𝓪\x20𝓟𝓪𝓷𝓪𝓹𝓲𝓽𝓲💫\x0a👨🏻‍💻⚟*ᴠᴇʀsɪᴏɴs*\x20:\x206.0.0\x20(ᴀᴅᴅᴇᴅ\x20ʜɪᴅᴅᴇɴ\x20ғᴇᴀᴜᴛᴜʀᴇs)💫\x0a👨🏻‍💻⚟\x20*ᴄᴏɴᴛᴀᴄᴛ*\x20:\x20https://wa.me/94757096717\x20💫\x0a\x0a╭───────────●●►\x0a│📜\x20MAIN\x20COMMANDS\x0a│───────\x0a\x20','917427InyxoU','428OFhhbw','../lib/database','length','\x0a╰───────────●●►\x0a\x20\x20\x20\x20'];_0x5026=function(){return _0x3a2eac;};return _0x5026();}const _0x5bf26f=_0x200f;function _0x200f(_0x49b346,_0x497f5f){const _0x50262f=_0x5026();return _0x200f=function(_0x200f47,_0x44e2c1){_0x200f47=_0x200f47-0x1a5;let _0x29034f=_0x50262f[_0x200f47];return _0x29034f;},_0x200f(_0x49b346,_0x497f5f);}(function(_0x2fd5ad,_0x3bc55f){const _0x282552=_0x200f,_0x2e397b=_0x2fd5ad();while(!![]){try{const _0xbb340d=parseInt(_0x282552(0x1bb))/0x1*(parseInt(_0x282552(0x1b6))/0x2)+parseInt(_0x282552(0x1be))/0x3+parseInt(_0x282552(0x1bf))/0x4*(parseInt(_0x282552(0x1a9))/0x5)+parseInt(_0x282552(0x1b9))/0x6+parseInt(_0x282552(0x1b0))/0x7*(parseInt(_0x282552(0x1b3))/0x8)+-parseInt(_0x282552(0x1a8))/0x9*(-parseInt(_0x282552(0x1b8))/0xa)+parseInt(_0x282552(0x1b4))/0xb*(-parseInt(_0x282552(0x1b1))/0xc);if(_0xbb340d===_0x3bc55f)break;else _0x2e397b['push'](_0x2e397b['shift']());}catch(_0x8003be){_0x2e397b['push'](_0x2e397b['shift']());}}}(_0x5026,0x290cc));const {readEnv}=require(_0x5bf26f(0x1a5)),{cmd,commands}=require('../command');cmd({'pattern':_0x5bf26f(0x1b2),'desc':'get\x20bot\x20main\x20cmd.','category':_0x5bf26f(0x1b2),'react':'😒','filename':__filename},async(_0x5d580e,_0x1e80f8,_0x5439d3,{from:_0x1572c2,quoted:_0x4c24a7,body:_0x38c6f6,isCmd:_0x40a4ee,command:_0x2b2e29,args:_0x4853ff,q:_0x5285b8,isGroup:_0x4cb19b,sender:_0x46982c,senderNumber:_0x57029b,botNumber2:_0x1a47ff,botNumber:_0x5f70ec,pushname:_0x4abd02,isMe:_0x1e246c,isOwner:_0x4b9e26,groupMetadata:_0x3c77d2,groupName:_0x412f2b,participants:_0x53d4a9,groupAdmins:_0xa8f4c2,isBotAdmins:_0x48e678,isAdmins:_0x2571f3,reply:_0x5efe43})=>{const _0x1df0a8=_0x5bf26f;try{const _0x5d9bfe=await readEnv();let _0x3d8420={'main':''};for(let _0x51228c=0x0;_0x51228c<commands[_0x1df0a8(0x1a6)];_0x51228c++){commands[_0x51228c][_0x1df0a8(0x1ae)]&&!commands[_0x51228c][_0x1df0a8(0x1ab)]&&(_0x3d8420[commands[_0x51228c][_0x1df0a8(0x1af)]]+=''+_0x5d9bfe[_0x1df0a8(0x1b7)]+commands[_0x51228c][_0x1df0a8(0x1ae)]+'\x0a');}let _0x1ab678='\x0a*👋\x20Hello\x20'+_0x4abd02+_0x1df0a8(0x1ba)+_0x5d9bfe[_0x1df0a8(0x1ad)]+_0x1df0a8(0x1b5)+_0x5d9bfe['BOT_NUMBER']+'\x0a*📝\x20Prefix:*\x20'+_0x5d9bfe['PREFIX']+_0x1df0a8(0x1bd)+_0x3d8420[_0x1df0a8(0x1b2)]+_0x1df0a8(0x1a7);await _0x5d580e[_0x1df0a8(0x1aa)](_0x1572c2,{'audio':{'url':_0x1df0a8(0x1ac)},'mimetype':'audio/mp4','ptt':!![]},{'quoted':_0x1e80f8}),await _0x5d580e[_0x1df0a8(0x1aa)](_0x1572c2,{'image':{'url':_0x1df0a8(0x1bc)},'caption':_0x1ab678},{'quoted':_0x1e80f8});}catch(_0x19ad22){console['log'](_0x19ad22),_0x5efe43(''+_0x19ad22);}});
