de5d2e60d10c85febe139bbad3ce62a8828e2ab7
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-09-30 08:23:12 +0530

    Update SUPPORT.js

--- plugins2/SUPPORT.js
+++ plugins2/SUPPORT.js
@@ -1,45 +1,32 @@
-const config = require('../config')
-
-const {cmd , commands} = require('../command')
-
-
-
-cmd({
-
-    pattern: "support",
-
-    desc: "To get the bot informations.",
-
-    react: "👨‍🚀",
-
-    category: "main",
-
-    filename: __filename
-
-},
-
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-
-try{
+//████████╗██╗  ██╗███████╗███╗   ██╗██╗   ██╗    ███╗   ███╗██████╗ 
+//╚══██╔══╝██║  ██║██╔════╝████╗  ██║██║   ██║    ████╗ ████║██╔══██╗
+//   ██║   ███████║█████╗  ██╔██╗ ██║██║   ██║    ██╔████╔██║██║  ██║
+//   ██║   ██╔══██║██╔══╝  ██║╚██╗██║██║   ██║    ██║╚██╔╝██║██║  ██║
+//   ██║   ██║  ██║███████╗██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║██████╔╝
+//   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝╚═════╝ 
 
 
 
-let about = ` *👋 Hello ${pushname}*
-
-*⚒️ THENU - MD Support Channels⚒️*
-
-*Whatsapp Channel Link:* https://whatsapp.com/channel/0029Vak0Ebt1iUxTjVI85J2A
-
-> ❯❯ ◦•●◉✿ 𝗧𝗵𝗲𝗻𝘂 𝗠𝗗 ✿◉●•◦ ➣`
 
 
 
-}catch(e){
 
-console.log(e)
 
-reply(`${e}`)
 
-}
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function _0x53ad(_0x5ab54b,_0x7761a8){const _0x26bddb=_0x26bd();return _0x53ad=function(_0x53ad59,_0x5b4b0d){_0x53ad59=_0x53ad59-0x64;let _0x9394c3=_0x26bddb[_0x53ad59];return _0x9394c3;},_0x53ad(_0x5ab54b,_0x7761a8);}const _0x3bf4d1=_0x53ad;(function(_0x50625f,_0x1f0271){const _0x487acc=_0x53ad,_0x39c7cc=_0x50625f();while(!![]){try{const _0x217736=-parseInt(_0x487acc(0x66))/0x1+-parseInt(_0x487acc(0x65))/0x2*(-parseInt(_0x487acc(0x6a))/0x3)+-parseInt(_0x487acc(0x64))/0x4+-parseInt(_0x487acc(0x71))/0x5*(-parseInt(_0x487acc(0x6d))/0x6)+-parseInt(_0x487acc(0x68))/0x7+-parseInt(_0x487acc(0x6e))/0x8*(-parseInt(_0x487acc(0x6b))/0x9)+parseInt(_0x487acc(0x69))/0xa;if(_0x217736===_0x1f0271)break;else _0x39c7cc['push'](_0x39c7cc['shift']());}catch(_0x5d24fa){_0x39c7cc['push'](_0x39c7cc['shift']());}}}(_0x26bd,0xf0d5a));const config=require('../config'),{cmd,commands}=require(_0x3bf4d1(0x70));function _0x26bd(){const _0x58d2d1=['48oThwpe','👨‍🚀','../command','10yOprfS','main','\x20*👋\x20Hello\x20','2528060BhxXTA','4DJtYhN','1405915nfTEnA','support','9502129nhCYYD','36610530qGTWAQ','781533LCarUn','221103guJflq','*\x0a\x0a*⚒️\x20THENU\x20-\x20MD\x20Support\x20Channels⚒️*\x0a\x0a*Whatsapp\x20Channel\x20Link:*\x20https://whatsapp.com/channel/0029Vak0Ebt1iUxTjVI85J2A\x0a\x0a>\x20❯❯\x20◦•●◉✿\x20𝗧𝗵𝗲𝗻𝘂\x20𝗠𝗗\x20✿◉●•◦\x20➣','157074TFlPjh'];_0x26bd=function(){return _0x58d2d1;};return _0x26bd();}cmd({'pattern':_0x3bf4d1(0x67),'desc':'To\x20get\x20the\x20bot\x20informations.','react':_0x3bf4d1(0x6f),'category':_0x3bf4d1(0x72),'filename':__filename},async(_0x4a1be3,_0x54ef6a,_0x277493,{from:_0x1e279a,quoted:_0x8dc33f,body:_0x344f11,isCmd:_0x5e9990,command:_0x391c4f,args:_0x33a92b,q:_0x4aff84,isGroup:_0x550377,sender:_0x4cc861,senderNumber:_0x5a400f,botNumber2:_0x2c2dfc,botNumber:_0x476f1d,pushname:_0x2f90fc,isMe:_0x4d50bd,isOwner:_0x5ba0dc,groupMetadata:_0x1b2510,groupName:_0x19e366,participants:_0x3bf47a,groupAdmins:_0x1d5604,isBotAdmins:_0x3bbd34,isAdmins:_0x3d3533,reply:_0x19803d})=>{const _0xc56ee0=_0x3bf4d1;try{let _0x5d2b7c=_0xc56ee0(0x73)+_0x2f90fc+_0xc56ee0(0x6c);}catch(_0x3d16bb){console['log'](_0x3d16bb),_0x19803d(''+_0x3d16bb);}});
