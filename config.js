154ef6001d56b2dc502d4ab990b5823834998c71
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-10-14 10:53:22 +0530

    ◦•●◉✿ 𝗧𝗵𝗲𝗻𝘂 𝗠𝗗 ✿◉●•◦

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter session id",
-MONGODB: process.env.MONGODB || "mongodb://mongo:GJyeHvVRytNLroiWrQnmdBGBbZSRAqFD@autorack.proxy.rlwy.net:58087",
+SESSION_ID: process.env.SESSION_ID || "EBAxHC7D#03YiwkXqf3-C_CQoHFpetJiwrlZO6iAIVPPSfdfIZdI",
+MONGODB: process.env.MONGODB || "mongodb://mongo:asvGZtHAxUOpWinxxNpCuOyLzcsqVMMi@junction.proxy.rlwy.net:31271",
 PREFIX: process.env.PREFIX || ".",
 BOT_NAME: process.env.BOT_NAME || "Thenu-MD",
 BOT_NUMBER: process.env.BOT_NUMBER || "94757096717",
